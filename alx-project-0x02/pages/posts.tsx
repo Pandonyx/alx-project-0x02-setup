0423c25f66c29a4245e2326d26137643654301ff
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-07-23 19:08:15 +0100

    getstatic and async api call

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,50 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PostCard from "@/components/common/PostCard";
 import type { PostProps } from "@/interfaces";
 import Header from "@/components/layout/Header";
 
-export default function PostsPage() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-  const [loading, setLoading] = useState(true);
+type PostsPageProps = {
+  posts: PostProps[];
+};
 
-  useEffect(() => {
-    // Fetch from JSONPlaceholder API
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
-      .then(res => res.json())
-      .then((data) => {
-        // Map API response to PostProps
-        const mapped = data.map((item: any) => ({
-          title: item.title,
-          content: item.body,
-          userId: item.userId,
-        }));
-        setPosts(mapped);
-        setLoading(false);
-      });
-  }, []);
-
+export default function PostsPage({ posts }: PostsPageProps) {
   return (
     <div>
-        <Header />
-    
-    <div className="p-8">
-      <h1 className="item-center flex justify-center text-2xl font-bold mb-6">Posts</h1>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className=" flex flex-wrap gap-4 justify-center">
+      <Header />
+      <div className="p-8">
+        <h1 className="flex justify-center text-2xl font-bold mb-6">Posts</h1>
+        <div className="flex flex-wrap gap-4 justify-center">
           {posts.map((post, index) => (
-            <div key={index} className="w-full sm:w-[320px] sm:lg:w-[360px]" >
-            <PostCard
-              title={post.title}
-              content={post.content}
-              userId={post.userId}
-            />
-          </div>
+            <div key={index} className="w-full sm:w-[320px] sm:lg:w-[360px]">
+              <PostCard
+                title={post.title}
+                content={post.content}
+                userId={post.userId}
+              />
+            </div>
           ))}
         </div>
-      )}
-    </div>
+      </div>
     </div>
   );
+}
+
+// SSG - fetches posts at build time
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+  const data = await res.json();
+  const posts: PostProps[] = data.map((item: any) => ({
+    title: item.title,
+    content: item.body,
+    userId: item.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
